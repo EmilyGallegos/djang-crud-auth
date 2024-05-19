56c567f815b10f1abdcc797b5d4076c641d0eb4f
Emily Gallegos <xmxly6@gmail.com>
2024-05-19 17:34:45 -0500

    first commit

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # exit on error
 set -o errexit
 # poetry install
-#gipip istall -r requirements.txt
+pip istall -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage-py migrate
